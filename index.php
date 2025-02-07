0e344884b9bd8cbc119b447ee1909e5ab7de51ca
mSnwyy <mparcisz@proton.me>
2025-02-07 20:17:39 +0100

    Add files via upload

--- index.php
+++ index.php
@@ -1,10 +1,13 @@
 <?php
-function sign_up() {
+session_start();
+
+function sign_up()
+{
     $conn = mysqli_connect('localhost', 'root', '', 'mal');
     if (!$conn) {
         die("Connection failed: " . mysqli_connect_error());
     }
-    
+
     $username = mysqli_real_escape_string($conn, $_POST['username']);
     $pass = mysqli_real_escape_string($conn, $_POST['pass']);
     $email = mysqli_real_escape_string($conn, $_POST['email']);
@@ -15,7 +18,7 @@ function sign_up() {
         echo "Username already exists.";
     } else {
         if (!empty($username) && !empty($pass) && !empty($email)) {
-    
+
             $hashed_pass = password_hash($pass, PASSWORD_DEFAULT);
 
             $insert_query = "INSERT INTO users (username, email, password_hash, created_at) VALUES ('$username', '$email', '$hashed_pass', NOW())";
@@ -32,7 +35,8 @@ function sign_up() {
     mysqli_close($conn);
 }
 
-function login() {
+function login()
+{
     $conn = mysqli_connect('localhost', 'root', '', 'mal');
     if (!$conn) {
         die("Connection failed: " . mysqli_connect_error());
@@ -40,15 +44,17 @@ function login() {
     $username = mysqli_real_escape_string($conn, $_POST['username']);
     $pass = mysqli_real_escape_string($conn, $_POST['pass']);
 
-    if(!empty($username) && !empty($pass)){
+    if (!empty($username) && !empty($pass)) {
 
-        $query = "SELECT password_hash FROM users WHERE username = '$username' LIMIT 1";
+        $query = "SELECT id, password_hash FROM users WHERE username = '$username' LIMIT 1";
         $result = mysqli_query($conn, $query);
 
         if (mysqli_num_rows($result) == 1) {
             $row = mysqli_fetch_assoc($result);
             $hashed_pass = $row['password_hash'];
             if (password_verify($pass, $hashed_pass)) {
+
+                $_SESSION['user_id'] = $row['id']; //storing user_id
                 echo "Login successful.";
             } else {
                 echo "Invalid password.";
@@ -69,4 +75,23 @@ if (isset($_POST['submit_signup'])) {
 if (isset($_POST['submit_login'])) {
     login();
 }
-?>
+
+function current_user() //user_id selection
+{
+    if (isset($_SESSION['user_id'])) {
+        $conn = mysqli_connect('localhost', 'root', '', 'mal');
+        if (!$conn) {
+            die("Connection failed: " . mysqli_connect_error());
+        }
+        $user_id = $_SESSION['user_id'];
+        echo $user_id;
+    }
+}
+
+function write_review()
+{
+    $conn = mysqli_connect('localhost', 'root', '', 'mal');
+    if (!$conn) {
+        die("Connection failed: " . mysqli_connect_error());
+    }
+}
